87b85944ff71fb1154cfa3274f178e8fcdf184c5
sparticvs <sparticvs@popebp.com>
2014-03-10 16:04:41 -0400

    Make basic Tornado-based WebSocket Server

--- src/buildgps_ws.py
+++ src/buildgps_ws.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
-####
+###
 # File: buildgps_ws.py
-# Desc: WebSocket Server for BuildGPS
+# Desc: BuildGPS WebSocket Proxy
 # Author(s): Charles `sparticvs' Timko <sparticvs@popebp.com>
-####
+###
 
-from flask import Flask
-# Import Flask-Sockets
+from tornado.web import Application
+from tornado.ioloop import IOLoop
+from tornado.websocket import WebSocketHandler
 
+import json
+import httplib
+
+
+class MainHandler(WebSocketHandler):
+    def open(self):
+        print "WebSocket Opened"
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print "WebSocket Closed"
+
+
+APP = Application([
+    (r"/buildgps", MainHandler),
+])
 
 if __name__ == "__main__":
-    # Run the Flask Server
+    APP.listen(8888)
+    IOLoop.instance().start()
